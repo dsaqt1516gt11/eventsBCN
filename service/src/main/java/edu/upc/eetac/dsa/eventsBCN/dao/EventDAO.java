1fc25c69839252e74bbcaec1fa4382052a01f46a
Aitor Piñeyro <Aitor@MacBook-Pro-de-Aitor.local>
2015-11-30 18:17:29 +0100

    anadido interface

--- service/src/main/java/edu/upc/eetac/dsa/eventsBCN/dao/EventDAO.java
+++ service/src/main/java/edu/upc/eetac/dsa/eventsBCN/dao/EventDAO.java
@@ -3,5 +3,6 @@ package edu.upc.eetac.dsa.eventsBCN.dao;
 /**
  * Created by juan on 30/11/15.
  */
-public class EventDAO {
+public interface EventDAO {
+
 }
--- service/src/main/java/edu/upc/eetac/dsa/eventsBCN/dao/EventDAOQuery.java
+++ service/src/main/java/edu/upc/eetac/dsa/eventsBCN/dao/EventDAOQuery.java
@@ -3,5 +3,5 @@ package edu.upc.eetac.dsa.eventsBCN.dao;
 /**
  * Created by juan on 30/11/15.
  */
-public class EventDAOQuery {
+public interface EventDAOQuery {
 }
