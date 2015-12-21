fcb94c8fef5c4e96167f06efa37e9724c1ddb263
Aitor Pineyro <itor95@gmail.com>
2015-12-21 11:54:38 +0100

    Nuevo intento

--- service/src/main/java/edu/upc/eetac/dsa/eventsBCN/dao/Database.java
+++ service/src/main/java/edu/upc/eetac/dsa/eventsBCN/dao/Database.java
@@ -6,6 +6,10 @@ import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 /**
  * Created by Aitor on 29/11/15.
@@ -15,7 +19,16 @@ public class Database {
     private DataSource ds;
 
     private Database() {
-        HikariConfig config = new HikariConfig(Database.class.getClassLoader().getResource("hikari.properties").getFile());
+        PropertyResourceBundle prb = (PropertyResourceBundle) ResourceBundle.getBundle("eventsBCN");
+        Enumeration<String> keys = prb.getKeys();
+        Properties properties = new Properties();
+        while(keys.hasMoreElements()){
+            String key = keys.nextElement();
+            properties.put(key, prb.getString(key));
+        }
+
+        //HikariConfig config = new HikariConfig(Database.class.getClassLoader().getResource("hikari.properties").getFile());
+        HikariConfig config = new HikariConfig(properties);
         ds = new HikariDataSource(config);
     }
 
